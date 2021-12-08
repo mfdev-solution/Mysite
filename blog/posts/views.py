541571c7c6f5c326c9db1e8f764e6f61a1f5d212
mfdev-solution <lasolution.mfdev@gmail.com>
2021-12-08 13:36:54 +0000

    the blog is updated

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Posts
 
 # Create your views here.
 
 
 def index(request):
-    return render(request , 'index.html')
+    posts = Posts.objects.all()
+    return render(request , 'index.html',{"posts":posts})
+
+def post(request, pk):
+    post = Posts.objects.get(id=pk)
+    return render(request , 'post.html',{'post':post})
